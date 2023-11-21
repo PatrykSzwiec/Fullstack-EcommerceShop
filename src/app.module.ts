ee44c39973e7e9111f76a0657e2b29a713058f57
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-11-21 01:52:48 +0100

    Create build to test page on render.com

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import {
   RequestMethod,
 } from '@nestjs/common';
 import * as cors from 'cors';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
@@ -17,9 +18,13 @@ import { CartModule } from './cart/cart.module';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
 import configuration from './config/configuration';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'client', 'build'),
+    }),
     CartModule,
     ProductModule,
     UsersModule,
@@ -36,7 +41,7 @@ import configuration from './config/configuration';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(cors({ origin: 'http://localhost:3000' })).forRoutes({
+    consumer.apply(cors()).forRoutes({
       path: '*',
       method: RequestMethod.ALL,
     });
